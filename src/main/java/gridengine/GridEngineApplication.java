a682b68c97b150f6baa253c122bb474597aa3136
Maksim Rudiak <maxim.rudiak@gmail.com>
2023-09-04 21:24:34 +0300

    feat: initial commit

---
+++ src/main/java/gridengine/GridEngineApplication.java
@@ -0,0 +1,13 @@
+package gridengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gridengine/GridEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package gridengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
