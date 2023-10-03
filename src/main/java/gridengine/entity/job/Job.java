f10d9384daa7826fb49ff493220190252a45624a
Maksim Rudiak <maxim.rudiak@gmail.com>
2023-10-03 20:25:19 +0300

    #5 add Job, JobState classes

--- src/main/java/gridengine/entity/job/Job.java
+++ src/main/java/gridengine/entity/job/Job.java
@@ -1,5 +1,49 @@
 package gridengine.entity.job;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * A common job description
+ */
+@Data
+@Builder
+@AllArgsConstructor
 public class Job {
 
+    /**
+     * Job`s id.
+     */
+    private int id;
+    /**
+     * Job`s priority.
+     */
+    private double priority;
+    /**
+     * Job`s name.
+     */
+    private String name;
+    /**
+     * Job`s owner.
+     */
+    private String owner;
+    /**
+     * Job`s state.
+     */
+    private JobState state;
+    /**
+     * Job`s submission date and time.
+     */
+    private LocalDateTime submissionTime;
+    /**
+     * A name of a queue in which current job was submitted.
+     */
+    private String queueName;
+    /**
+     * Number of slots that the job takes up.
+     */
+    private int slots;
 }
---
+++ src/main/java/gridengine/entity/job/JobState.java
@@ -0,0 +1,42 @@
+package gridengine.entity.job;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * This class represents job states.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobState {
+
+    /**
+     * Job`s category.
+     */
+    private Category category;
+    /**
+     * Job`s state.
+     */
+    private String state;
+    /**
+     * Job`s state code.
+     */
+    private String stateCode;
+
+    /**
+     * Possible job`s categories.
+     * To get more information about grid engine possible category values see <a href="https://manpages.debian.org/testing/gridengine-common/sge_status.5.en.html">
+     * Grid Engine category values </a>
+     */
+    public enum Category {
+        PENDING,
+        RUNNING,
+        SUSPENDED,
+        ERROR,
+        DELETED,
+        FINISHED,
+        UNKNOWN
+    }
+}
