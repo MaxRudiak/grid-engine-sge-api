3201061743655a8e8931f1e7b3a79b2703c94ac0
Maksim Rudiak <maxim.rudiak@gmail.com>
2023-10-18 20:15:06 +0300

    issue #5 - add JaxbUtils class - add SgeQueueListing class

---
+++ src/main/java/gridengine/provider/utils/JaxbUtils.java
@@ -0,0 +1,24 @@
+package gridengine.provider.utils;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.io.StringReader;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class JaxbUtils {
+
+    public static <T> T unmarshall(final String xml, final Class<T> clazz) {
+
+        try{
+            final JAXBContext context = JAXBContext.newInstance(clazz);
+            final Unmarshaller unmarshaller = context.createUnmarshaller();
+            return (T) unmarshaller.unmarshal(new StringReader(xml));
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
