6f32b0018d77197bd3f20d79dd3b8c4de5e655e3
Maksim Rudiak <maxim.rudiak@gmail.com>
2024-03-13 21:54:06 +0300

    issue #21 - finish QsubCommandParser

--- src/main/java/gridengine/provider/utils/QsubCommandParser.java
+++ src/main/java/gridengine/provider/utils/QsubCommandParser.java
@@ -1,12 +1,18 @@
 package gridengine.provider.utils;
 
 import gridengine.entity.job.JobOptions;
+import gridengine.entity.job.ParallelEnv;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import sun.util.resources.ext.CalendarData_da;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class QsubCommandParser {
@@ -39,10 +45,74 @@ public final class QsubCommandParser {
         if (!isValidJob(options)) {
             throw new IllegalStateException("Exit code: 1; Error output: Invalid jobOptions structure");
         }
-
+        final List<String> commands = new ArrayList<>();
+        commands.add(COMMAND_BODY);
+        if (options.isCanBeBinary()) {
+            commands.add(BINARY);
+            commands.add(IS_BINARY);
+        }
+        if (options.isUseAllEnvVars()) {
+            commands.add(USE_ALL_VARS);
+        }
+        if (options.getName() != null && !options.getName().isEmpty()) {
+            commands.add(COMMAND_NAME);
+            commands.add(options.getName());
+        }
+        if (options.getWorkingDir() != null && !options.getWorkingDir().isEmpty()) {
+            commands.add(WORK_DIR);
+            commands.add(options.getWorkingDir());
+        }
+        if (options.getStdErrPath() != null && !options.getStdErrPath().isEmpty()) {
+            commands.add(ERR_PATH);
+            commands.add(options.getStdErrPath());
+        }
+        if (options.getStdOutPath() != null && !options.getStdOutPath().isEmpty()) {
+            commands.add(OUT_PATH);
+            commands.add(options.getStdOutPath());
+        }
+        if (options.getPriority() != 0) {
+            commands.add(PRIORITY);
+            commands.add(Integer.toString(options.getPriority()));
+        }
+        if (options.getQueues() != null && options.getQueues().isEmpty()) {
+            commands.add(QUEUES);
+            final StringBuilder queues = new StringBuilder();
+            options.getQueues().forEach(q -> queues.append(q).append(COMMA));
+            commands.add(queues.toString());
+        }
+        if (options.getEnvVariables() != null && !options.getEnvVariables().isEmpty()) {
+            commands.add(SET_VARIABLE);
+            final Map<String, String> envVarMap = options.getEnvVariables();
+            commands.add(getVariablesStringFromMap(envVarMap));
+        }
+        final ParallelEnv parallelEnv = options.getParallelEnv();
+        if (parallelEnv != null) {
+            commands.add(PARALLEL_ENV);
+            commands.add(parallelEnv.getName() == null
+                    ? EMPTY_STRING
+                    : parallelEnv.getName());
+            final String params = parallelEnv.getMin()
+                    + (parallelEnv.getMax() != 0
+                    ? "-" + parallelEnv.getMax()
+                    : EMPTY_STRING);
+            commands.add(params);
+        }
+        commands.add(options.getCommand().trim());
+        if (options.getArguments() != null && !options.getArguments().isEmpty()) {
+            commands.addAll(options.getArguments());
+        }
+        return commands.toArray(new String[0]);
     }
 
     private static boolean isValidJob(final JobOptions options) {
         return options.getCommand() != null && !options.getCommand().isEmpty();
+    }
+
+    private static String getVariablesStringFromMap(final Map<String, String> varMap) {
+        return varMap.entrySet().stream()
+                .map(e -> e.getKey() + (!e.getValue().isEmpty()
+                        ? "=" + e.getValue()
+                        : EMPTY_STRING))
+                .collect(Collectors.joining(COMMA));
     }
 }
